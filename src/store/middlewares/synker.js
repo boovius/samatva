1d4c6536d2374aab440bc6321d2fc5379d689960
Joshua Book <joshua.book@gmail.com>
2017-07-14 17:46:06 -0400

    comments for future addressing - synks constants array to synker and hot module update

--- src/store/middlewares/synker.js
+++ src/store/middlewares/synker.js
@@ -1,9 +1,15 @@
 import { ADD_ACTIVITY } from '../../constants/'
 import { usersRef } from '../../config/firebase'
 
+//const SYNCS = [
+//  ADD_ACTIVITY,
+//  DO_ACTIVITY,
+//]
+
 const synker = store => next => action => {
   let result;
 
+  //if (action.type includedIn(SYNCS)) {
   if (action.type === ADD_ACTIVITY) {
     result = next(action)
 
