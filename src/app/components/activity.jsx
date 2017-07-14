9ddcb17da9f7aaf4b11d8d41ac17acbb30967b5e
Joshua Book <joshua.book@gmail.com>
2017-07-14 17:45:20 -0400

    add doings count and doActivity functionality to activity component

--- src/app/components/activity.jsx
+++ src/app/components/activity.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { doActivity } from '../../actions';
 
 export class Activity extends React.Component {
   render() {
+    const { activity } = this.props;
     return (
       <div className='activity'>
-        <div className='title'>{this.props.title}</div>
-        <div className='do'>+</div>
+        <div className='title'>{activity.title}</div>
+        <div className='doingsCount'>{doingsCount(activity)}</div>
+        <div className='do' onClick={this.props.doActivity}>+</div>
       </div>
     )
+  }
+}
+
+function doingsCount(activity) {
+  if (typeof activity.doings != 'undefined') {
+    return activity.doings.length;
+  } else {
+    return 0;
   }
 }
 
@@ -16,8 +27,10 @@ function mapStateToProps() {
   return {}
 }
 
-function mapDispatchToProps() {
+function mapDispatchToProps(dispatch, ownProps) {
+  const { activity } = ownProps;
   return {
+    doActivity: () => { dispatch(doActivity(activity.id, Date.now())) }
   }
 }
 
