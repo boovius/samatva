52472871281a5c56940172c3ca3b6f2f177665e1
Joshua Book <joshua.book@gmail.com>
2017-07-27 09:50:46 -0400

    update heroku deploy script to point to new and correct app

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 export $(cat .env | xargs)
-NODE_ENV=production webpack -p && heroku static:deploy --app react-fire
+NODE_ENV=production webpack -p && heroku static:deploy --app samatva
 
